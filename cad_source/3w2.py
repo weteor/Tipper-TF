57c06787d26abd7bf51d52e30c85991d60bc0d37
weteor <github@wetreg.fastmail.net>
2021-09-11 23:53:55 +0200

    angled case

--- cad_source/3w2.py
+++ cad_source/3w2.py
@@ -13,6 +13,8 @@ from typing import (TypeVar)
 explode = False
 explode_val = 30
 
+changeLower = False
+
 mc = reload(mc)
 bg = reload(bg)
 cfgs = reload(cfgs)
@@ -72,9 +74,11 @@ plate, oLine_pcb = bg.GeneratePlate(pCfg)
 
 def generateCt(plate, oLine_pcb, cCfg, pCfg):
     
+    topExtra = 4
+    
     filletS = 0.3
     filletL = 1
-    chamferS = 0.3
+    chamferS = 0.25
     
     tButtons = len(pCfg.tClusterRot)
     
@@ -88,13 +92,20 @@ def generateCt(plate, oLine_pcb, cCfg, pCfg):
                         .extrude(-(cCfg.heightAbovePlate+cCfg.switchClearance+cCfg.bottomThickness+cCfg.clearanceSafety))
                 )
     
+    gndFace = ct.faces(">Z").tag("gndFace")
+    
+    ct = ct.faces(tag="gndFace").wires().first().toPending().extrude(topExtra)
+    merger = cq.Workplane().transformed(offset=(0,0,cCfg.heightAbovePlate)).rect(300,150).extrude(-20).rotateAboutCenter((1,0,0), 1.5)
+    
+    ct = ct.intersect(merger)
+    
     selector = cq.selectors.BoxSelector( (-300,-20,-1), (+300,-100,20))
     invSelector = cq.selectors.InverseSelector(selector)
-    ct= ct.faces(">Z").wires().first().chamfer(2,2.5)
-    # return ct, None, None
+
+    ct= ct.faces(">Z").wires().first().chamfer(2.5)
+    
     # ct = ct.faces(">Z").wires().first().chamfer(2,2.5)
-    # ct = ct.faces(">Z").wires().first().fillet(2)
-    ct = ct.faces("<<Z[-2]").edges(">Z").fillet(1)
+    # ct = ct.faces("<<Z[-2]").edges(">Z").fillet(1)
 
     outline = oLine_pcb.faces(">Z").wires().first()
     cutout = ( cq.Workplane().add(outline).translate((0,0,-cCfg.switchPlateToPcb))
@@ -145,24 +156,22 @@ def generateCt(plate, oLine_pcb, cCfg, pCfg):
                          .rect(pCfg.spacing[0] * pCfg.tClusterSize[tButtons-1-i] + cCfg.cutoutExtra[1] * 2,
                                pCfg.spacing[1] + cCfg.cutoutExtra[1] * 2)
                          )
-    cutout = cutout.extrude(cCfg.heightAbovePlate+cCfg.switchPlateToPcb)
+    cutout = cutout.extrude(cCfg.heightAbovePlate+cCfg.switchPlateToPcb+topExtra)
 
-    selector = cq.selectors.BoxSelector( (-300,0,-1), (0,15,cCfg.heightAbovePlate+1))
+    selector = cq.selectors.BoxSelector( (-300,0,-1), (0,15,cCfg.heightAbovePlate+1+topExtra))
     invSelector = cq.selectors.InverseSelector(selector)
     cutout = cutout.edges(invSelector).edges("|Z").fillet(filletL)
-    # ct = ct.cut(cutout.)
+
     cutout = cutout.edges(selector).edges("|Z").fillet(filletS)
 
-    # pointytop = cutout.edges(selector).edges("|Z").vertices(">Z").rect(5,5).extrude(5)
-    # pointytop = 
-    # square = 
-    # return ct, None, None
     ct = ct.cut(cutout.mirror("YZ",union=True))
     
-
-    for i in [2,5]:
-        ct = ct.faces(">Z").wires().item(i+1).chamfer(chamferS)
-        
+    sel = cq.selectors.NearestToPointSelector((0,0,8))
+    ct.faces(sel).tag("Top")
+    
+    for i in [1,2,3,4,5,6]:
+        ct = ct.faces(tag="Top").wires().item(i).chamfer(chamferS)
+    
     pos = ct.faces("-Z").faces("<Z").edges("<Y").vertices("<X").val().toTuple()
     ct = (ct.faces("-Z").faces(">Z")
                         .pushPoints([(pos[0]-7, pos[1]+34.25)])
@@ -179,7 +188,6 @@ def generateCt(plate, oLine_pcb, cCfg, pCfg):
    
     ct = ct.pushPoints(locs).circle(cCfg.hDiameter/2).cutBlind(cCfg.hDepth)
     bottomCase = bottomCase.faces("<Z").workplane().pushPoints(locsbc).hole(cCfg.sHoleDiameter)#.cboreHole(cCfg.sHoleDiameter,2*cCfg.sHoleDiameter,0.75)
-    
     
     locs = []
     loc = ct.faces("-Z").faces("<<Z[-3]").faces(">Y").first().val().Center()
@@ -209,19 +217,16 @@ def generateCt(plate, oLine_pcb, cCfg, pCfg):
 
 ct, cb, pcb = generateCt(plate, oLine_pcb, cCfg, pCfg)
 
-loc = cq.Location(cq.Vector((0,-20,0)))
+loc = cq.Location(cq.Vector((0,-20,20)))
 
-ct = ct.faces(">Z").workplane().pushPoints([loc]).hole(33)
-ct = ct.faces(">Z").wires(cq.selectors.NearestToPointSelector((0,-20,0))).chamfer(0.4)
+cutout = cq.Workplane().pushPoints([loc]).circle(16.5).extrude(-50)
+ct = ct.cut(cutout)
+
+
+# ct = ct.faces(">Z").workplane().pushPoints([loc]).hole(33)
+ct = ct.faces(tag="Top").wires(cq.selectors.NearestToPointSelector((0,-20,0))).chamfer(0.25)
 plate = plate.faces(">Z").workplane().pushPoints([loc]).hole(32)
 
-# pcb_outline = plate.faces("<Z").wires().first().val()
-# pcb = (cq.Workplane().add(pcb_outline).wires().first()
-#                      .toPending()
-#                      .offset2D(-(cCfg.wallThickness+pCfg.holeToEdge-2))
-#                      .extrude(-pCfg.height_pcb)
-#                      .translate((0,0,-(pCfg.height_plate+pCfg.height_mid)))
-#                      )
 outline_case = plate.faces(">Z")
 combine = (cq.Workplane().add(outline_case).wires().toPending()
                         .extrude(-20)
@@ -229,8 +234,9 @@ combine = (cq.Workplane().add(outline_case).wires().toPending()
 
 combine = combine.intersect(pcb)
 
-loc = ((0, -cCfg.heightAbovePlate-cCfg.switchPlateToPcb, -cCfg.wallThickness))
+
 locc = ct.faces("+Y").faces(">>Y[-2]").val().Center()
+loc = ((0, -(locc.z + cCfg.switchPlateToPcb), -cCfg.wallThickness))
 ct = ct.faces("+Y").faces(">>Y[-2]").workplane().pushPoints([loc]).rect(12,6).cutBlind(cCfg.wallThickness)
 ct = ct.edges("|Y").fillet(1)
 
@@ -258,200 +264,7 @@ ct = ct.faces(tag="theFace").rect(31,41).cutBlind(-5.2)
 # ct = ct.faces(tag="theFace").rect(30,60,(True, False)).extrude(1.5)
 # ct = ct.faces(tag="theFace").rect(35,14).cutBlind(-5.2)
 
-ct = ct.faces("<Z").wires().first().chamfer(0.5)
-
-thumbEdgeShift = 10
-cutLength = 20
-zOffset = cq.Vector(0,0,0)
-
-sel=cq.selectors.BoxSelector((-40,-30,5),(0,-80, 2))
-#wires = ct.faces(sel).edges(">>Z[-6]")
-wires = ct.faces(sel).edges(">>Z[-5]")
-
-sel = cq.selectors.NearestToPointSelector((-50,-40,0))
-startedge = ct.faces(">Z").wires(sel).edges("<<X[-2]")
-ct = ct.faces(">Z").wires(sel).chamfer(0.3)
-sel = cq.selectors.NearestToPointSelector((50,-40,0))
-ct = ct.faces(">Z").wires(sel).chamfer(0.3)
-
-
-p11  = startedge.vertices("<X").val().Center()
-p12  = startedge.vertices(">X").val().Center()
-p11s = p11+cq.Vector(thumbEdgeShift*math.sin(math.radians(90+21)), thumbEdgeShift*math.sin(math.radians(90+21)))
-p12s = p12+cq.Vector(thumbEdgeShift*math.sin(math.radians(90+21)), thumbEdgeShift*math.sin(math.radians(90+21)))
-p21 = wires.edges("<<X[-1]").vertices("<X").val().Center() + zOffset
-p22 = cq.Vector(p21.x + math.sin(math.radians(270+21)), p21.y + math.cos(math.radians(270+21)), p21.z ) + zOffset
-
-Intpnt = getInterceptPoint2D(p11s,p12s,p21,p22)
-contactLow = wires.edges("<<X[-1]").vertices("<X").val().Center() + zOffset
-contactHigh = p11 + cq.Vector(28.7*math.sin(math.radians(90+21)),28.7*math.cos(math.radians(90+21)),0)
-
-verts = []
-verts.append(contactLow)
-verts.append(cq.Vector(Intpnt[0], Intpnt[1], p21.z))
-verts.append(p11)
-verts.append(contactHigh)
-
-wirelist = []
-for i in range(len(verts)):
-    wirelist.append(cq.Edge.makeLine(verts[i], verts[(i+1)%len(verts)]))
-
-cutout = cq.Workplane(cq.Wire.assembleEdges(wirelist)).wires().first().toPending().extrude(5).mirror("YZ", union=True)
-ct = ct.cut(cutout)
-
-
-verts = []
-verts.append(cq.Vector(Intpnt[0], Intpnt[1], p21.z))
-verts.append(p11)
-verts.append(p11- cq.Vector(cutLength*math.sin(math.radians(90+21)),cutLength*math.cos(math.radians(90+21)),-2) - zOffset)  
-verts.append(cq.Vector(Intpnt[0], Intpnt[1], p21.z) - cq.Vector(cutLength*math.sin(math.radians(90+21)),cutLength*math.cos(math.radians(90+21)),-2) - zOffset)
-# verts.append(ct.faces(sel).vertices(">Z").vertices("<Y").val().Center() - cq.Vector(60*math.sin(math.radians(90+21)),60*math.cos(math.radians(90+21))))
-
-wirelist = []
-for i in range(len(verts)):
-    wirelist.append(cq.Edge.makeLine(verts[i], verts[(i+1)%len(verts)]))
-    
-debug(wirelist)
-cutout = cq.Workplane(cq.Wire.assembleEdges(wirelist)).wires().first().toPending().extrude(5).mirror("YZ", union=True)
-ct = ct.cut(cutout)
-
-
-
-sel = cq.selectors.NearestToPointSelector((-20,-50,0))
-startedge = ct.faces(">Z").wires(sel).edges(">>X[-2]")
-ct = ct.faces(">Z").wires(sel).chamfer(0.3)
-sel = cq.selectors.NearestToPointSelector((20,-50,0))
-ct = ct.faces(">Z").wires(sel).chamfer(0.3)
-
-p11 = startedge.vertices("<X").val().Center()
-p12 = startedge.vertices(">X").val().Center()
-p21 = wires.edges("<<X[-1]").vertices("<X").val().Center() + zOffset
-p22 = wires.edges("<<X[-1]").vertices(">X").val().Center() + zOffset
-
-Intpnt = cq.Vector(*getInterceptPoint2D(p11,p12,p21,p22),p21.z)
-
-verts = []
-verts.append(contactHigh)
-verts.append(p11)
-verts.append(p22)
-# verts.append(cq.Vector(p21.x, p21.y, p22.z))
-
-wirelist = []
-for i in range(len(verts)):
-    wirelist.append(cq.Edge.makeLine(verts[i], verts[(i+1)%len(verts)]))
-
-cutout = cq.Workplane(cq.Wire.assembleEdges(wirelist)).wires().first().toPending().extrude(5).mirror("YZ", union=True)
-ct = ct.cut(cutout)
-
-verts = []
-verts.append(contactHigh)
-# verts.append(p11)
-verts.append(p22)
-verts.append(cq.Vector(p21.x, p21.y, p22.z))
-
-wirelist = []
-for i in range(len(verts)):
-    wirelist.append(cq.Edge.makeLine(verts[i], verts[(i+1)%len(verts)]))
-    
-cutout = cq.Workplane(cq.Wire.assembleEdges(wirelist)).wires().first().toPending().extrude(5).mirror("YZ", union=True)
-ct = ct.cut(cutout)
-
-
-p21 = wires.edges("<<X[-3]").vertices("<X").val().Center() + zOffset
-p22 = wires.edges("<<X[-3]").vertices(">X").val().Center() + zOffset
-
-verts = []
-verts.append(p21)
-verts.append(p11)
-verts.append(p11+cq.Vector(20,0,0))
-verts.append(p21+cq.Vector(20,0,0))
-
-wirelist = []
-for i in range(len(verts)):
-    wirelist.append(cq.Edge.makeLine(verts[i], verts[(i+1)%len(verts)]))
-    
-cutout = cq.Workplane(cq.Wire.assembleEdges(wirelist)).wires().first().toPending().extrude(5).mirror("YZ", union=True)
-ct = ct.cut(cutout)
-
-contactLow = Intpnt
-contactHigh = p11 
-
-wirelist = []
-# wirelist.append(wires.edges("<<X[-2]").val())
-wirelist.append(cq.Edge.makeLine(wires.edges("<<X[-2]").vertices(">X").val().Center() + zOffset, 
-                                 wires.edges("<<X[-2]").vertices("<X").val().Center() + zOffset))
-wirelist.append(cq.Edge.makeLine(wires.edges("<<X[-2]").vertices("<X").val().Center() + zOffset, p11))
-wirelist.append(cq.Edge.makeLine(p11, wires.edges("<<X[-2]").vertices(">X").val().Center() + zOffset))
-
-cutout = cq.Workplane(cq.Wire.assembleEdges(wirelist)).wires().first().toPending().extrude(5).mirror("YZ", union=True)
-ct = ct.cut(cutout)
-
-wirelist = []
-# wirelist.append(wires.edges("<<X[-2]").val())
-wirelist.append(cq.Edge.makeLine(wires.edges("<<X[-2]").vertices(">X").val().Center() + zOffset, 
-                                 wires.edges("<<X[-2]").vertices("<X").val().Center() + zOffset))
-wirelist.append(wires.edges("<<X[-2]").val().translate(zOffset))
-
-cutout = cq.Workplane(cq.Wire.assembleEdges(wirelist)).wires().first().toPending().extrude(5).mirror("YZ", union=True)
-ct = ct.cut(cutout)
-
-
-
-# midpnt = ct.faces("<<Y[-13]").edges(">Z").vertices("<X").val().Center()
-
-# sel = cq.selectors.NearestToPointSelector((-50,-40,0))
-# startpntL = ct.faces(">Z").edges(sel).vertices("<X").val().Center()
-
-# sel = cq.selectors.NearestToPointSelector((-20,-50,0))
-# startpntR = ct.faces(">Z").edges(sel).vertices("<X").val().Center()
-
-# cutout = ( cq.Workplane()
-#              .transformed(offset = startpntL)
-#              .transformed(offset = cq.Vector(1*math.sin(math.radians(270+21)),1*math.cos(math.radians(270+21)),0), 
-#                           rotate = cq.Vector(0,0,-21))
-#              .transformed(offset = cq.Vector(0,1,0), rotate = cq.Vector(13.1,0,0))
-#              )
-# midpnt_local = cutout.plane.toLocalCoords(midpnt)
-# pnts = []
-# pnts.append( (0,0) )
-# pnts.append( (28.68,0) )
-# pnts.append( ( midpnt_local.x, midpnt_local.y))
-#                # pnts[-1][1]+ 10*math.cos(math.radians(180+7.5))))
-# pnts.append( ( 10*math.sin(math.radians(180)), #+15)),
-#                -1+ 10*math.cos(math.radians(180))))#~+15))))
-# pnts.append( (0,-1) )
-# debug(cutout.polyline(pnts).close())
-# ct = ct.cut(cutout.polyline(pnts).close().extrude(5).mirror("YZ", union=True))
-
-
-# cutout = ( cq.Workplane()
-#               .transformed(offset = startpntR)
-#               .transformed(offset = cq.Vector(1*math.sin(math.radians(270+36)),1*math.cos(math.radians(270+36)),0), 
-#                             rotate = cq.Vector(0,0,-36))
-#               .transformed(offset = cq.Vector(0,1,0), rotate = cq.Vector(13.1,0,0))
-#               )
-
-# midpnt_local = cutout.plane.toLocalCoords(midpnt)
-# pnts = []
-# pnts.append( (0,0) ) 
-# pnts.append( (-1.18,0))
-# pnts.append( (midpnt_local.x, midpnt_local.y))
-# pnts.append( ( 18.5+ 10*math.sin(math.radians(180)),#-15)),
-#               -1 + 10*math.cos(math.radians(180))#-15))
-#               )
-#             )
-# pnts.append( (18.5,0) )
-# debug(cutout.polyline(pnts).close())
-
-# ct = ct.cut(cutout.polyline(pnts).close().extrude(5).mirror("YZ", union=True))
-
-
-# debug(cutout.polyline(pnts).close())
-
-
-# # debug(cutout.rotateAboutCenter((1,0,0), 13.25).extrude(5).rotateAboutCenter((0,0,1),-21).translate((math.sin(math.radians(21)),math.cos(math.radians(21)))))
-# ct = ct.cut(cutout.rotateAboutCenter((1,0,0), 10.6).extrude(5).rotateAboutCenter((0,0,1),-21).translate((3*math.sin(math.radians(21)),3*math.cos(math.radians(21)))))
-
+ct = ct.faces("<Z").wires().first().chamfer(0.75)
 
 pos = combine.faces(">Z").edges("|X").edges(">>Y[-3]").vertices("<X").val().Center()
 
@@ -465,12 +278,8 @@ locs.append((0,locs[-1][1]))
 combine = combine.faces(">Z").workplane().polyline(locs).mirrorY().extrude(-pCfg.height_pcb)
 combine = combine.cut(ct)
 
-# t = cq.Workplane().pushPoints([(0,-35)]).rect(17.55,23.35)
-# ct = ( ct.faces("-Z").faces("<<Z[-3]").workplane()
-#           .add(t).translate((0,-12.5,-cCfg.switchPlateToPcb)).toPending().extrude(4.2)
-          # )
 # show_object(combine, name="Combine", options={"color":(30,30,30)})
 show_object(cb, name="Case_bottom", options={"color":(198,196,188)})
 show_object(ct, name="Case_top", options={"color":(100,196,188)})
-# show_object(pcb, name="pcb", options={"color":(30,30,30)})
+# # show_object(pcb, name="pcb", options={"color":(30,30,30)})
 
